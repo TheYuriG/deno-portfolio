857e13add6dd58e6e75336a8deac0d2ba6cdb450
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-05-16 19:09:48 -0300

    docs: better semantics, more attributes/comments

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,16 +13,18 @@ export default function Home() {
 				<link rel="stylesheet" href="file.css" />
 			</Head>
 			<body>
-				<div class="absolute flex flex-row -left-14 bottom-24 text-xl -rotate-90 ml-2 mb-2 space-x-2">
-					<label for="dark">
-						<input class="mr-1" type="radio" id="dark" name="theme" checked></input>
-						Dark
-					</label>
-					<label for="light">
-						<input class="mr-1" type="radio" id="light" name="theme"></input>
-						Light
-					</label>
-				</div>
+				<aside>
+					<div class="absolute flex flex-row -left-14 bottom-24 text-xl -rotate-90 ml-2 mb-2 space-x-2">
+						<label for="dark">
+							<input class="mr-1" type="radio" id="dark" name="theme" checked></input>
+							Dark
+						</label>
+						<label for="light">
+							<input class="mr-1" type="radio" id="light" name="theme"></input>
+							Light
+						</label>
+					</div>
+				</aside>
 				<main class="absolute top-10 left-10 bottom-10 right-10 p-4 border-grey border-2">
 					<h1 class="inline-block text-4xl">Hello!</h1>
 					<p class="block text-2xl ml-4">
