e9bc24c7e863cc966f988335448293a80bf0871e
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-05-18 20:12:43 -0300

    fix: allow Base to only receive 1 JSX.Element

--- components/Base.tsx
+++ components/Base.tsx
@@ -5,9 +5,9 @@ import { Aside } from '../components/Aside.tsx';
 //? Footer with tech stack
 import { Footer } from '../components/Footer.tsx';
 //? Import Preact JSX so we can define the props.children type
-import { JSX } from 'preact';
+import { JSX, toChildArray } from 'preact';
 
-export function Base(props: { children: Array<JSX.Element> }) {
+export function Base(props: { children: Array<JSX.Element> | JSX.Element }) {
 	return (
 		<body>
 			<MetaHead />
@@ -18,7 +18,7 @@ export function Base(props: { children: Array<JSX.Element> }) {
 				class="absolute top-10 left-10 bottom-10 right-10 p-4"
 				style="border: 2px solid var(--accent-color); transition: border 0.7s ease-in-out 0.3s"
 			>
-				{...props.children}
+				{...toChildArray(props.children)}
 			</main>
 			{/* Footer with Tech Stack on bottom right corner */}
 			<Footer />
