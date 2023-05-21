0cc88d5d68867224717328c1a70ae6c09d0d74cc
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-05-21 18:07:09 -0300

    feat: add Starry Night to the Home Page

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,6 +6,8 @@ import { HomeContent } from "../components/HomeContent.tsx";
 import { CustomHead } from "../components/CustomHead.tsx";
 //? Navigation Menu with redirects to different pages
 import { NavigationMenu } from "../components/NavigationMenu.tsx";
+//? Create a Starry Night background
+import { StarryNight } from "../components/StarryNight.tsx";
 
 export default function Home() {
   return (
@@ -16,10 +18,14 @@ export default function Home() {
         link="https://www.theyurig.com/"
       >
         <link rel="stylesheet" href="home.css" />
+        <link rel="stylesheet" href="/starry-night.css" />
         <link rel="stylesheet" href="/gradient-underline.css" />
       </CustomHead>
+      {/* Starry Night background <3 */}
+      <StarryNight />
       {/* Base page layout with theme switching and footer outside of accent box */}
       <Base>
+        <div style="background-color: var(--base-color);"></div>
         {/* Content with greeting, name, workplace */}
         <HomeContent />
         {/* Navigation menu to other routes */}
