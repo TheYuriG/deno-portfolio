ba07acbd7e00a121ae0b8f7f2c0bb9b4e4586c07
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-05-22 10:18:59 -0300

    feat: create blog index

---
+++ components/BlogPostSummary.tsx
@@ -0,0 +1,20 @@
+//? Require the interface to ensure we receive the proper data
+import BlogPostSummaryProperties from "../types/BlogPostSummaryProperties.ts";
+
+//? Exports a single Blog Post Summary
+export function BlogPostSummary(summary: BlogPostSummaryProperties) {
+  return (
+    // What a Post Summary looks like
+    <article class="blog-post" style="margin: 1em 0">
+      {/* Post link */}
+      <a href={"/blog" + summary.link}>
+        {/* Centered heading */}
+        <h2 class="navigation-link blog-title">{summary.title}</h2>
+      </a>
+      {/* Post creation date */}
+      <p class="post-date">{new Date(summary.date).toLocaleString()}</p>
+      {/* Post summary */}
+      <p class="justified">{summary.shortSummary}</p>
+    </article>
+  );
+}
--- routes/blog.tsx
+++ routes/blog.tsx
@@ -1,7 +1,13 @@
 //? Lateral text with theme switcher
 import { Base } from "../components/Base.tsx";
+//? Import the template for Blog Post summaries
+import { BlogPostSummary } from "../components/BlogPostSummary.tsx";
 //? Head component with all Meta tags pre-set
 import { CustomHead } from "../components/CustomHead.tsx";
+//? Navigation Buttons to go back to the previous page or to the next page (optional)
+import BlogNavigationButtons from "../islands/BlogNavigationButtons.tsx";
+//? All posts so far
+import createdPosts from "../blog_posts/all-blog-posts.ts";
 
 export default function Home() {
   return (
@@ -12,11 +18,23 @@ export default function Home() {
         link="https://www.theyurig.com/blog"
       >
         <link rel="stylesheet" href="home.css" />
-        <link rel="stylesheet" href="/starry-night.css" />
-        <link rel="stylesheet" href="/gradient-underline.css" />
+        <link rel="stylesheet" href="navigation-buttons.css" />
+        <link rel="stylesheet" href="content.css" />
+        <link rel="stylesheet" href="blog.css" />
       </CustomHead>
       {/* Base page layout with theme switching and footer outside of accent box */}
       <Base>
+        {/* Back button */}
+        <BlogNavigationButtons />
+        <section class="center">
+          {...createdPosts.map((post) => (
+            <BlogPostSummary {...post}></BlogPostSummary>
+          ))}
+          <footer class="blog-footer">
+            Disclaimer: I haven't created any of these posts myself, just using
+            them as mock data to test the layout.
+          </footer>
+        </section>
       </Base>
     </>
   );
