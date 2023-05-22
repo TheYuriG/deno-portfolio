2aa512b44f034586bb32c01bf3c5c1db845c5104
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-05-22 04:54:43 -0300

    chore: add title attr to all Home links

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -4,19 +4,28 @@ export function NavigationMenu() {
       class="flex flex-col p-4 items-end absolute bottom-0 right-0"
       style="font-family: 'Alfa Slab One', cursive;"
     >
-      <a href="/work" class="navigation-link">
+      <a href="/work" class="navigation-link" title="Relevant work experience">
         Work
       </a>
-      <a href="/toys" class="navigation-link">
+      <a href="/toys" class="navigation-link" title="Curiosity projects">
         Toys
       </a>
-      <a href="/blog" class="navigation-link">
+      <a
+        href="/blog"
+        class="navigation-link"
+        title="Development findings shared over the years"
+      >
         Blog
       </a>
-      <a href="/me" class="navigation-link">
+      <a href="/me" class="navigation-link" title="Who is Yuri?">
         Me
       </a>
-      <a href="/this" class="text-sm" style="color: yellow; margin-top: 2em;">
+      <a
+        href="/this"
+        class="text-sm"
+        style="color: yellow; margin-top: 2em;"
+        title="information about how this website is built"
+      >
         this
       </a>
     </nav>
