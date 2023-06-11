86883a924a5dbf3c3b8589943828f0ba82a45c20
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-11 05:55:01 -0300

    refactor: migrated 'loading' animation to twind

--- twind.config.ts
+++ twind.config.ts
@@ -9,8 +9,12 @@ export default {
         "card-pulse": "card-pulse 1.2s ease-out infinite",
         // Insanity grow and shrink
         grow: "grow 1.2s ease-out infinite",
+        // Waving animation for hello emoji in the home page
         waving: "waving 0.3s linear infinite alternate;",
+        // blinking animation
         blinking: "blinking 1.5s step-end infinite",
+        // Loading spinner animation
+        loading: "loading-spinner 1.8s infinite ease-in-out",
       },
       keyframes: {
         // Toys card pulsing animation
@@ -45,12 +49,30 @@ export default {
             "transform": "translate(-10%, 10%) rotate(0deg)",
           },
         },
+        // Blinking animation
         blinking: {
           "0%, 100%": {
             opacity: 0,
           },
           "50%": {
             opacity: 1,
+          },
+        },
+        // Loading spinner animation
+        "loading-spinner": {
+          "0%": {
+            "stroke-dasharray": "1, 98",
+            "stroke-dashoffset": "-105",
+          },
+
+          "50%": {
+            "stroke-dasharray": "80, 15",
+            "stroke-dashoffset": "-160",
+          },
+
+          "100%": {
+            "stroke-dasharray": "1, 98",
+            "stroke-dashoffset": "-300",
           },
         },
       },
