40b64f3a5e79f4cf27d16e824292a1d02458c1e3
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-14 06:41:10 -0300

    chore: renamed file/function to reflect updates

--- middleware/toys/__syntax-highlight.tsx
+++ middleware/toys/__syntax-highlight.tsx
@@ -1,17 +1,17 @@
 //? Import handlers to manage HTTP methods for GET/POST to /toys/syntax-highlight
 import { Handlers } from "$fresh/server.ts";
-//? Import service that handles saving temporary JSON files to disk and then deleting them later
-import { saveHighlightTextToDisk } from "../../services/syntax-highlight/save-highlight-text-to-disk.ts";
+//? Import service that handles saving data to KV and then deleting them later
+import { manageHighlightTextOnKv } from "../../services/syntax-highlight/manage-highlight-text-on-KV.ts";
 
 //? Export a middleware responsible for handling incoming GET/POST requests to /toys/syntax-highlight
 export const syntaxHighlightMiddleware: Handlers = {
   //? If the user is trying to load the page, display it
   GET(req, ctx) {
     const errors = new URL(req.url).searchParams.get("error") || "";
-    console.log(errors);
     return ctx.render({ errors });
   },
-  //? When the user attempts to subtmit the highlighted text, temp save it to the file system to load it on the next route
+  //? When the user attempts to submit the highlighted text, save it to
+  //? Deno KV, which later gets loaded on the next route
   async POST(req, ctx) {
     const form = await req.formData();
     const text = form.get("text-to-highlight")?.toString();
@@ -20,25 +20,21 @@ export const syntaxHighlightMiddleware: Handlers = {
 
     //! throws error, needs better status code?
     //* user error, confirmed sending empty form
-    if (text === "") {
-      headers.set("location", "/toys/syntax-highlight");
-      return new Response(null, {
-        status: 400, // Bad Request HTTP status code
+    if (text === "" || text === undefined) {
+      headers.set(
+        "location",
+        "/toys/syntax-highlight?error=Empty%2Finvalid%20form%20provided!",
+      );
+      return new Response("Error! You attempted to submit an empty form!", {
+        status: 303, // Bad Request HTTP status code
         headers,
       });
     }
 
-    //! This will only happen if the user attempts to send a form outside of the page or
-    //! using external tools and failing to send a proper form
-    //? Returns a "not found" page
-    if (text === undefined) {
-      return ctx.renderNotFound();
-    }
-
     try {
       const now = new Date().getTime();
-      await saveHighlightTextToDisk(text, now);
-      // Redirect to highlighted text page
+      await manageHighlightTextOnKv(text, now);
+
       headers.set("location", "/toys/highlighted-text/" + now.toString());
       return new Response(null, {
         status: 303, // See Other HTTP status code
--- services/syntax-highlight/manage-highlight-text-on-KV.ts
+++ services/syntax-highlight/manage-highlight-text-on-KV.ts
@@ -1,27 +1,30 @@
 //? Import the error type for whenever there is a failed attempt to save to disk
+import { deleteFromKv } from "../../data/deleteFromKv.ts";
+import { saveToKv } from "../../data/saveToKv.ts";
 import FetchDataError from "../../types/error/FetchDataError.ts";
 
 //? Function to save text to disk so it can be read by another route
-export async function saveHighlightTextToDisk(text: string, now: number) {
+export async function manageHighlightTextOnKv(text: string, now: number) {
   //? Instantiate variables that will be reused
-  const fileExpiration = now + 3600000;
+  const ONE_HOUR_IN_MS = 3600000;
+  const fileExpiration = now + ONE_HOUR_IN_MS;
   const dataToSave = { createdAt: now, expiresAt: fileExpiration, text };
   const filePath = `./temp/${now}.json`;
 
   //? Attempt to save a file with our data to disk
   try {
-    await Deno.writeTextFile(filePath, JSON.stringify(dataToSave));
+    await saveToKv(["highlight", now.toString()], dataToSave);
   } catch (error) {
     //? If the save fails, throw an error to load the error page
-    throw new FetchDataError("Failed to save file to disk!", error);
+    throw new FetchDataError("Failed to save file to KV!", error);
   }
 
   //? After saving the file to disk, create a timer to automatically delete the file
   //? from disk after 'fileExpiration' passed
   try {
     setTimeout(() => {
-      Deno.removeSync(filePath);
-    }, fileExpiration);
+      deleteFromKv(["highlight", now.toString()]);
+    }, ONE_HOUR_IN_MS);
   } catch (error) {
     //? If the deletion fails, there is no need to throw an error because it wouldn't
     //? render an error page. The user had already been long redirected to another page
