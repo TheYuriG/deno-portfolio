2153225036ca650a411e6c489ae00b9a2b1c45de
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-14 12:37:40 -0300

    feat: display error message for bad submissions

--- routes/[toys]/syntax-highlight.tsx
+++ routes/[toys]/syntax-highlight.tsx
@@ -1,4 +1,5 @@
 //? Create blog content inside Base component
+import { ErrorAlert } from "../../components/UI/ErrorAlert.tsx";
 import { Base } from "../../components/base/Base.tsx";
 //? Head component with all Meta tags pre-set
 import { CustomHead } from "../../components/base/CustomHead.tsx";
@@ -29,20 +30,21 @@ export default function Home(
           back={{ title: "Return to all toys", link: "/toys" }}
         />
         <section class="flex flex-col h-full w-full max-w-4xl mx-auto items-center">
+          {/* Error message, if necessary */}
+          {errors.length > 0 && <ErrorAlert errorText={errors} />}
           {/* Title */}
           <h1 class="custom-underline-thick hover:custom-tx-ac f-as my-4 text-2xl lg:text-4xl text-center">
             Syntax Highlighting as HTML+CSS
           </h1>
           {/* Introduction */}
           <p class="mb-4">
-            Insert your text below and click "Highlight!" the text. After
-            submitting, a page will be created that hosts the highlighted HTML
-            code for the next 60 minutes, before getting deleted from the
-            database.
+            Insert your text below and click "Highlight!". After submitting, a
+            page will be created that hosts the highlighted HTML code for the
+            next 60 minutes, before getting deleted from the database.
           </p>
           {/* Syntax highlight input + result */}
           <SyntaxHighlighForm />
-          <p className="mt-4">
+          <p class="mt-4">
             You can share or revisit the highlighted snippet as many times as
             you want within those 60 minutes. There is no limit to how many
             snippets you might create, just make sure you are not submitting any
