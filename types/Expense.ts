02b6232835e4733625c4cd965a3e132a6bab3827
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-17 22:06:22 -0300

    chore: expenses date is now a number of ms

--- types/Expense.ts
+++ types/Expense.ts
@@ -1,6 +1,6 @@
 //? All the data an Expense is required to have
 export interface Expense {
-  date: Date;
+  date: number;
   description: string;
   cost: number;
 }
--- types/expenses-tracker/mockExpenses.ts
+++ types/expenses-tracker/mockExpenses.ts
@@ -1,27 +1,29 @@
+import { Expense } from "../Expense.ts";
+
 //? Mock expenses
-export const mockExpenses = [
+export const mockExpenses: Expense[] = [
   {
-    date: new Date(2020, 1, 1),
+    date: new Date(2020, 1, 1).getTime(),
     description: "Medical Insurance",
     cost: 3600,
   },
   {
-    date: new Date(2020, 5, 3),
+    date: new Date(2020, 5, 3).getTime(),
     description: "Shoes",
     cost: 200,
   },
   {
-    date: new Date(2020, 8, 18),
+    date: new Date(2020, 8, 18).getTime(),
     description: "House",
     cost: 250000,
   },
   {
-    date: new Date(2020, 9, 20),
+    date: new Date(2020, 9, 20).getTime(),
     description: "Car",
     cost: 40000,
   },
   {
-    date: new Date(2020, 11, 28),
+    date: new Date(2020, 11, 28).getTime(),
     description: "Kids",
     cost: 10000,
   },
