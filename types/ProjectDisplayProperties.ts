830532fb5891b7457dc049cd972419a4942e7478
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-19 06:41:51 -0300

    feat: projects are stacked in grid with preview

---
+++ types/ProjectDisplayProperties.ts
@@ -0,0 +1,7 @@
+//? Define what obligatory data the component must have
+export interface ProjectDisplayProperties {
+  projectLink: string;
+  projectTitle: string;
+  projectImageLink: string;
+  projectName: string;
+}
