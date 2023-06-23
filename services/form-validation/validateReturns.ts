8fe0fdd27454ee2ced57bf10146d67b3d994f57d
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-23 06:09:57 -0300

    refactor: code splitting

--- services/form-validation/validateReturns.ts
+++ services/form-validation/validateReturns.ts
@@ -1,11 +1,11 @@
 //? Types for typecasting
 import { validationStatus } from "../../types/misc/validationStatus.ts";
 
-//? Validates the form's investment numeric input field
-export function validateYearlyInvestment(
+//? Validates the form's returns numeric input field
+export function validateReturns(
   investment: string,
 ): validationStatus.Invalid | validationStatus.Valid {
-  if (Number(investment) >= 10 && Number(investment) <= 100) {
+  if (Number(investment) >= 1) {
     return validationStatus.Valid;
   } else {
     return validationStatus.Invalid;
---
+++ types/retirement-calculator/baseRetirementStats.ts
@@ -0,0 +1,8 @@
+//? Starting values for the form
+export const baseRetirementStats = {
+  currentAge: "18",
+  plannedRetiringAge: "30",
+  compensation: "20000",
+  yearlySavings: "10",
+  returns: "6",
+};
