1c027169a62f347a5ac8a1a5b33c9b5afd60120b
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-06-30 06:19:23 -0300

    feat: added React certificate

--- routes/certificates.tsx
+++ routes/certificates.tsx
@@ -8,13 +8,14 @@ import { StyledHeader } from "../components/UI/StyledHeader.tsx";
 import { GradientLink } from "../components/UI/GradientLink.tsx";
 //? Navigation Buttons to go back to the previous page or to the next article
 import { NavigationButtons } from "../components/misc/NavigationButtons.tsx";
+import { StyledSubHeader } from "../components/UI/StyledSubHeader.tsx";
 
 export default function Home() {
   return (
     <>
       <CustomHead
-        title="Various certificates earned while completing courses"
-        description="A small gallery of my feats."
+        title="Tech stack certificates that I've earned for completing online courses"
+        description="A gallery of my feats."
         link="https://www.theyurig.com/certificates"
       >
       </CustomHead>
@@ -23,20 +24,59 @@ export default function Home() {
         <NavigationButtons />
         <article class="flex flex-col h-full w-full max-w-4xl mx-auto items-center">
           <StyledHeader title="Certificates" />
-          {/* Udemy backend NodeJS certificate */}
+          {/* NodeJS backend certificate */}
+          <StyledSubHeader title="NodeJS (Backend focused)" />
           <img
             src="https://udemy-certificate.s3.amazonaws.com/image/UC-c14620b0-6803-48a3-a7ab-5211825cec51.jpg?v=1685285730000"
             alt="NodeJS Backend Certificate"
             title="NodeJS Backend completion Certificate"
-            class="my-4 object-cover max-h-[20em]"
+            class="object-cover max-h-[30em] max-w-full"
+            style="aspect-ratio: 43/32;"
           />
-          <p class="text-center w-full">
+          <GradientLink
+            link="https://www.udemy.com/certificate/UC-c14620b0-6803-48a3-a7ab-5211825cec51/"
+            content="View on Udemy"
+            title="Certificate of the Node.JS backend course completion on Udemy."
+            customRel="noopener noreferrer"
+          />
+          <p class="mt-2 mb-4 text-center text-justify w-full">
+            This course enabled me building both (previously using Node/JS,
+            currently with Deno/TS) backends of{" "}
             <GradientLink
-              link="https://www.udemy.com/certificate/UC-c14620b0-6803-48a3-a7ab-5211825cec51/"
-              content="View on Udemy"
-              title="Certificate of the Node.JS backend course completion on Udemy."
+              content="Trophy Place"
+              link="https://trophy.place"
+              newTab={true}
+            />.
+          </p>
+          <StyledSubHeader title="React (+NextJS)" />
+          {/* React and NextJS certificate */}
+          <img
+            src="https://udemy-certificate.s3.amazonaws.com/image/UC-7246cba0-de77-4a6f-81a4-2be5aaeaad67.jpg?v=1688062528000"
+            alt="React18 Certificate"
+            title="React and NextJS completion Certificate"
+            class="object-cover max-h-[30em] max-w-full"
+            style="aspect-ratio: 43/32;"
+          />
+          <GradientLink
+            link="https://www.udemy.com/certificate/UC-7246cba0-de77-4a6f-81a4-2be5aaeaad67/"
+            content="View on Udemy"
+            title="Certificate of the ReactJS course completion on Udemy."
+            customRel="noopener noreferrer"
+          />
+          <p class="mt-2 mb-4 text-center text-justify w-full">
+            This course helped me brushing up my React skillset. Although I had
+            8 months of prior freelance experience with React when I started
+            this course, I learned a lot of fundamentals that I didn't know and
+            I now feel that I have a very strong foundation of how React works
+            internally because of this. I was able to make some improvements to
+            this website because I finally learned how to apply{" "}
+            <span class="shl-inline">useRef</span> effectively instead of{" "}
+            <GradientLink
+              link="https://github.com/TheYuriG/deno-portfolio/commit/6ee8aab60851f80217a70f72e293ca13e173f203#comments"
+              content="using pure Javascript"
+              title="GitHub link to the related commit"
               customRel="noopener noreferrer"
-            />
+            />.
           </p>
         </article>
       </Base>
