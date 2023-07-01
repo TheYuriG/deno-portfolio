bb8b3f5a0926d2f824bd52728019049fd3861d1b
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-01 17:50:57 -0300

    feat: comments, allow textarea value to be pulled

--- components/UI/StyledTextArea.tsx
+++ components/UI/StyledTextArea.tsx
@@ -1,12 +1,20 @@
 interface StyledTextAreaProperties {
+  //? Links text area to parent form
   name?: string;
+  //? What should be displayed as label
   label?: string;
+  //? String that links the textarea and the label
   labelLink: string;
+  //? Additional classes to style the label
   labelClasses?: string;
+  //? Value to display when no input is provided
   placeholder?: string;
   minHeight?: string;
-  disabled?: boolean;
-  textContent?: string;
+  disabled?: true;
+  //? Initial/current value for this input
+  value?: string;
+  //? Function that updates the input state when typing in the input field
+  inputFunction?: (input: string) => void;
 }
 
 export function StyledTextArea(
@@ -17,8 +25,9 @@ export function StyledTextArea(
     labelClasses = "",
     placeholder,
     minHeight = "20em",
-    disabled = false,
-    textContent = "",
+    disabled,
+    value = "",
+    inputFunction,
   }: StyledTextAreaProperties,
 ) {
   return (
@@ -32,11 +41,22 @@ export function StyledTextArea(
       <textarea
         class={`w-full min-h-[${minHeight}] p-2 custom-bg-bc custom-bo-ac custom-tx-nc custom-placeholder-nc custom-tr-tx-bg-bo rounded-2xl styled-scrollbar`}
         name={name}
-        id={labelLink}
-        disabled={disabled}
+        id={labelLink} //? Link to label
+        disabled={disabled} //? Determines if
+        //? Value to display when no input is provided
         placeholder={placeholder}
+        //? Updates the value on typing
+        onInput={(e) => {
+          if (inputFunction !== undefined) {
+            const { target } = e;
+            if (target) {
+              const changedValue = (target as HTMLTextAreaElement).value;
+              inputFunction(changedValue);
+            }
+          }
+        }}
       >
-        {textContent}
+        {value}
       </textarea>
     </div>
   );
