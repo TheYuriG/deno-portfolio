c9c92eba852948c9ce4e4ab672ecf0a5d16ab990
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-02 18:21:14 -0300

    refactor: more images on /work, pruned boring text

--- routes/work/index.tsx
+++ routes/work/index.tsx
@@ -24,6 +24,11 @@ export default function Home() {
         <article class="flex flex-col h-full w-full max-w-4xl mx-auto items-center">
           {/* Title header */}
           <StyledHeader title="Past and Current work" />
+          <img
+            src="https://images-ext-2.discordapp.net/external/MrGU7E1Vw1ks2LSlDu4fiYOrT8wgVB4fCfoxRii6SrE/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/555126737038737408/a61db2e4c1f0a92cacee54f7b1aea624.png?width=554&height=554"
+            alt="Yura's latest avatar"
+            class="my-4 mx-auto h-40 w-40"
+          />
           <p class="my-2 text-justify">
             I am the creator of{" "}
             <GradientLink
@@ -32,15 +37,18 @@ export default function Home() {
               link="https://discordapp.com/invite/j55v7pD"
               title="My Discord bot's support server."
             />, a PSN-related Discord bot, based on PSNProfiles.com data
-            (2018-2023). I've also{" "}
-            <GradientLink
-              content="blogged"
-              newTab={true}
-              link="https://github.com/TheYuriG/blog_lessons/tree/master/discord_create_channels"
-              title="My Medium post about how to create various things in Discord.JS v14."
-            />{" "}
-            about Discord.JS in the past, it has an amazing API.
+            (2018-2023). I maintained it and updated it weekly until they
+            blocked all third-party requests to the website.
           </p>
+          <iframe
+            class="my-4"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/V0en08tuceY"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          >
+          </iframe>
           <p class="my-2 text-justify">
             I've created{" "}
             <GradientLink
@@ -66,9 +74,14 @@ export default function Home() {
             />{" "}
             (2020).
           </p>
+          <img
+            src="https://media.discordapp.net/attachments/576538316296421399/1125169376157519963/image.png?width=832&height=671"
+            alt="Yura's port to mobile/desktop"
+            class="my-4 mx-auto w-full"
+          />
           <p class="my-2 text-justify">
             In the following year, I started porting Yura's functionality from
-            Discord to a{"  "}
+            Discord to a{" "}
             <GradientLink
               content="mobile/desktop app"
               newTab={true}
@@ -80,42 +93,19 @@ export default function Home() {
             Eventually I shelved that project (2021).
           </p>
           <p class="my-2 text-justify">
-            After freelancing for design agencies for a while, in 2022 I decided
-            to eventually move out of freelancing and try to get a remote job in
-            a tech company. Using Wix/SquareSpace/Framer templates to build
-            websites was really wearing me off, since we were just building
-            contract websites as fast as possible only to start the next. Just
-            felt like the lack of testing, UX and post-launch care for the
-            product wasn't like how WebDev was meant to be.
+            I've been working as a contractor for design agencies since early
+            2022, but using Wix/SquareSpace/Framer templates to build websites
+            is really wearing me off, since we were just building contract
+            websites as fast as possible only to start the next and repeat it
+            all over again. It just doesn't feel like WebDev is meant to be.
           </p>
+          <img
+            src="https://media.discordapp.net/attachments/576538316296421399/1125171715916443678/image.png?width=920&height=670"
+            alt="Trophy Place games page"
+            class="my-4 mx-auto w-full"
+          />
           <p class="my-2 text-justify">
-            I wasn't feeling like I was making any progress in becoming a
-            serious full-stack developer, so I've bought some{" "}
-            <GradientLink
-              content="Udemy courses"
-              newTab={true}
-              link="https://www.udemy.com/certificate/UC-c14620b0-6803-48a3-a7ab-5211825cec51/"
-              customRel="noopener noreferrer"
-              title="My Node.JS backend certificate"
-            />{" "}
-            and actually become a more interesting potential employee.
-          </p>
-          <p class="my-2 text-justify">
-            That got me to land some interviews, I even made some{" "}
-            <GradientLink
-              content="take-home assignments"
-              newTab={true}
-              link="https://github.com/TheYuriG/fullstackTodoApp"
-              customRel="noopener noreferrer"
-              title="A React Native assignment"
-            />{" "}
-            and experienced React Native for the first time (not wildly
-            different from Flutter), but didn't really land anything other than
-            more freelance work and some temporary contracts.
-          </p>
-          <p class="my-2 text-justify">
-            Since I wasn't having much luck getting professionally into tech, in
-            2023 I focused on fleshing out older projects I had, eventually
+            I've been focusing on fleshing out older projects I had, eventually
             starting{" "}
             <GradientLink
               content="Trophy Place"
@@ -123,27 +113,19 @@ export default function Home() {
               link="https://my.trophy.place/"
               title="More than just a passion project, Trophy Place will eventually become the defacto website for trophy hunting on PSN."
             />. My partner took over the frontend and I take care of the
-            backend, which is currently written with Node.JS and Express, but I
-            want to eventually rewrite it with Deno and Oak instead. Currently
-            Trophy Place, this website and freelancing are my daily routine.
+            backend, which is currently written with Node.JS and Express, but
+            will be rewritten it with Deno and Oak.
           </p>
           <p class="my-2 text-justify">
-            This website has been specially useful into getting me to learn how
-            to build things from scratch, like these:
+            Currently, Trophy Place, this website you are currently visiting,
+            studying and my agency contract jobs are my daily{" "}
+            <GradientLink
+              content="coding routine"
+              newTab={true}
+              link="https://github.com/TheYuriG"
+              title="My GitHub profile page."
+            />.
           </p>
-          <ol
-            start={1}
-            class="self-start list-[lower-greek]"
-          >
-            <li class="ml-10 lg:ml-0 transition-[margin-left] ease-in-out duration-500">
-              <GradientLink
-                content="Form (with validation)"
-                title="A simple mock data form."
-                link="/work/form"
-                newTab={false}
-              />
-            </li>
-          </ol>
         </article>
       </Base>
     </>
