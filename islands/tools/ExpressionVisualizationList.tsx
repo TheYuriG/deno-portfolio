fdfc3358bf651c57ef3c43c9eebaea146f6cb3af
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-07 22:59:17 -0300

    feat: dynamically update expression on click

--- islands/tools/ExpressionVisualizationList.tsx
+++ islands/tools/ExpressionVisualizationList.tsx
@@ -1,4 +1,6 @@
-//todo State management
+//? JSX to define array properties
+import type { JSX } from "preact";
+//? State management
 import { useState } from "preact/hooks";
 //? Component
 import { ExpressionVisualizationListItem } from "../../components/tools/ExpressionVisualizationListItem.tsx";
@@ -9,12 +11,57 @@ import type { visualizer } from "../../types/component-properties/tools/expressi
 export default function ExpressionVisualizationList(
   { visualizationList }: { visualizationList: visualizer[] },
 ) {
+  //? Instantiate array that will hold all the animation states
+  const playStateArray: boolean[] = [];
+  visualizationList.forEach(() => {
+    console.log("New state created");
+    playStateArray.push(false);
+  });
+  const [shouldAnimationPlay, toggleAnimationPlayStatus] = useState(
+    playStateArray,
+  );
+
+  //? Instantiate current loop index
+  let listIndex = 0;
+
+  //? Create an array of Visualization JSX elements that gets rendered
+  const visualizationListElements: JSX.Element[] = visualizationList.map(
+    (listItem) => {
+      //? Instantiate current index to search the arrays
+      //! If not instantiated, all indexes will have the value after the last loop
+      const currentIndex = listIndex;
+      //? Pull the animation status state for this expression
+      const animationStatus = shouldAnimationPlay[currentIndex];
+
+      //? Create update function for this expression. It will only update the
+      //? animation state of itself
+      const updateFunction = () => {
+        toggleAnimationPlayStatus((currentStatuses) => {
+          const updatedStatuses = [...currentStatuses];
+          updatedStatuses[currentIndex] = !updatedStatuses[currentIndex];
+          return updatedStatuses;
+        });
+      };
+
+      //? Increase index to start over for the next component
+      listIndex++;
+
+      //? Create an Expression item for this object
+      return (
+        <ExpressionVisualizationListItem
+          {...listItem}
+          playState={animationStatus}
+          updateFunction={updateFunction}
+        />
+      );
+    },
+  );
+
+  //? Render a list with all Expressions to be displayed
   return (
     <div class="flex flex-col w-full items-center">
       {/* Generate steps dynamically */}
-      {visualizationList.map((visualizationItem) => (
-        <ExpressionVisualizationListItem {...visualizationItem} />
-      ))}
+      {...visualizationListElements}
     </div>
   );
 }
