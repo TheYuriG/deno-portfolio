a19854edad1465a80294cc4ec7a8051279eba3ea
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-11 18:16:18 -0300

    feat: added optional id property to headers

--- components/UI/StyledHeader.tsx
+++ components/UI/StyledHeader.tsx
@@ -1,7 +1,10 @@
 //? Export a header that contains all common classes for easy styling
-export function StyledHeader({ title }: { title: string }) {
+export function StyledHeader({ title, id }: { title: string; id?: string }) {
   return (
-    <h1 class="custom-underline-thick custom-font-scale-largest f-as my-4 text-center">
+    <h1
+      id={id}
+      class="custom-underline-thick custom-font-scale-largest f-as my-4 text-center"
+    >
       {title}
     </h1>
   );
