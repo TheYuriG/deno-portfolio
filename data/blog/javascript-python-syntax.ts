ee01bddf15d5245912313ca8a07a4161ccf85105
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-07-12 20:32:52 -0300

    prototype: javascript/python syntax blog post

---
+++ data/blog/javascript-python-syntax.ts
@@ -0,0 +1,11 @@
+//? Import required properties for a blog post
+import type { BlogPostSummaryProperties } from "../../types/blog/BlogPostSummaryProperties.ts";
+
+//? Export object with blog post summary data
+export const javascriptPythonSyntaxPost: BlogPostSummaryProperties = {
+  title: "Syntax Cheatsheet for Javascript/Python developers",
+  shortSummary:
+    "A quick refresher for an experienced developer in either Javascript or Python that just needs to find how you do X from one language in the other.",
+  date: 1689154833983,
+  link: "/blog/javascript-python-syntax",
+};
--- fresh.gen.ts
+++ fresh.gen.ts
@@ -11,27 +11,28 @@ import * as $5 from "./routes/blog/how-create-text-channels-discord-v14.tsx";
 import * as $6 from "./routes/blog/how-create-theme-switcher-deno-fresh.tsx";
 import * as $7 from "./routes/blog/how-create-voice-channels-discord-v14.tsx";
 import * as $8 from "./routes/blog/index.tsx";
-import * as $9 from "./routes/blog/stopping-theme-flickering-deno-fresh.tsx";
-import * as $10 from "./routes/certificates.tsx";
-import * as $11 from "./routes/index.tsx";
-import * as $12 from "./routes/me.tsx";
-import * as $13 from "./routes/projects/expenses-tracker.tsx";
-import * as $14 from "./routes/projects/food-order/checkout.tsx";
-import * as $15 from "./routes/projects/food-order/index.tsx";
-import * as $16 from "./routes/projects/food-order/success.tsx";
-import * as $17 from "./routes/projects/index.tsx";
-import * as $18 from "./routes/projects/stimulus-check.tsx";
-import * as $19 from "./routes/tools/expression-visualizer.tsx";
-import * as $20 from "./routes/tools/highlighted-text/[text].tsx";
-import * as $21 from "./routes/tools/index.tsx";
-import * as $22 from "./routes/tools/retirement-calculator.tsx";
-import * as $23 from "./routes/tools/syntax-highlight.tsx";
-import * as $24 from "./routes/tools/whatsapp-message-link-generator.tsx";
-import * as $25 from "./routes/toys/index.tsx";
-import * as $26 from "./routes/toys/insanity.tsx";
-import * as $27 from "./routes/toys/spinners.tsx";
-import * as $28 from "./routes/what-is-this.tsx";
-import * as $29 from "./routes/work/index.tsx";
+import * as $9 from "./routes/blog/javascript-python-syntax.tsx";
+import * as $10 from "./routes/blog/stopping-theme-flickering-deno-fresh.tsx";
+import * as $11 from "./routes/certificates.tsx";
+import * as $12 from "./routes/index.tsx";
+import * as $13 from "./routes/me.tsx";
+import * as $14 from "./routes/projects/expenses-tracker.tsx";
+import * as $15 from "./routes/projects/food-order/checkout.tsx";
+import * as $16 from "./routes/projects/food-order/index.tsx";
+import * as $17 from "./routes/projects/food-order/success.tsx";
+import * as $18 from "./routes/projects/index.tsx";
+import * as $19 from "./routes/projects/stimulus-check.tsx";
+import * as $20 from "./routes/tools/expression-visualizer.tsx";
+import * as $21 from "./routes/tools/highlighted-text/[text].tsx";
+import * as $22 from "./routes/tools/index.tsx";
+import * as $23 from "./routes/tools/retirement-calculator.tsx";
+import * as $24 from "./routes/tools/syntax-highlight.tsx";
+import * as $25 from "./routes/tools/whatsapp-message-link-generator.tsx";
+import * as $26 from "./routes/toys/index.tsx";
+import * as $27 from "./routes/toys/insanity.tsx";
+import * as $28 from "./routes/toys/spinners.tsx";
+import * as $29 from "./routes/what-is-this.tsx";
+import * as $30 from "./routes/work/index.tsx";
 import * as $$0 from "./islands/UI/DigitalTimer.tsx";
 import * as $$1 from "./islands/UI/Modal.tsx";
 import * as $$2 from "./islands/misc/Collapsible.tsx";
@@ -62,27 +63,28 @@ const manifest = {
     "./routes/blog/how-create-theme-switcher-deno-fresh.tsx": $6,
     "./routes/blog/how-create-voice-channels-discord-v14.tsx": $7,
     "./routes/blog/index.tsx": $8,
-    "./routes/blog/stopping-theme-flickering-deno-fresh.tsx": $9,
-    "./routes/certificates.tsx": $10,
-    "./routes/index.tsx": $11,
-    "./routes/me.tsx": $12,
-    "./routes/projects/expenses-tracker.tsx": $13,
-    "./routes/projects/food-order/checkout.tsx": $14,
-    "./routes/projects/food-order/index.tsx": $15,
-    "./routes/projects/food-order/success.tsx": $16,
-    "./routes/projects/index.tsx": $17,
-    "./routes/projects/stimulus-check.tsx": $18,
-    "./routes/tools/expression-visualizer.tsx": $19,
-    "./routes/tools/highlighted-text/[text].tsx": $20,
-    "./routes/tools/index.tsx": $21,
-    "./routes/tools/retirement-calculator.tsx": $22,
-    "./routes/tools/syntax-highlight.tsx": $23,
-    "./routes/tools/whatsapp-message-link-generator.tsx": $24,
-    "./routes/toys/index.tsx": $25,
-    "./routes/toys/insanity.tsx": $26,
-    "./routes/toys/spinners.tsx": $27,
-    "./routes/what-is-this.tsx": $28,
-    "./routes/work/index.tsx": $29,
+    "./routes/blog/javascript-python-syntax.tsx": $9,
+    "./routes/blog/stopping-theme-flickering-deno-fresh.tsx": $10,
+    "./routes/certificates.tsx": $11,
+    "./routes/index.tsx": $12,
+    "./routes/me.tsx": $13,
+    "./routes/projects/expenses-tracker.tsx": $14,
+    "./routes/projects/food-order/checkout.tsx": $15,
+    "./routes/projects/food-order/index.tsx": $16,
+    "./routes/projects/food-order/success.tsx": $17,
+    "./routes/projects/index.tsx": $18,
+    "./routes/projects/stimulus-check.tsx": $19,
+    "./routes/tools/expression-visualizer.tsx": $20,
+    "./routes/tools/highlighted-text/[text].tsx": $21,
+    "./routes/tools/index.tsx": $22,
+    "./routes/tools/retirement-calculator.tsx": $23,
+    "./routes/tools/syntax-highlight.tsx": $24,
+    "./routes/tools/whatsapp-message-link-generator.tsx": $25,
+    "./routes/toys/index.tsx": $26,
+    "./routes/toys/insanity.tsx": $27,
+    "./routes/toys/spinners.tsx": $28,
+    "./routes/what-is-this.tsx": $29,
+    "./routes/work/index.tsx": $30,
   },
   islands: {
     "./islands/UI/DigitalTimer.tsx": $$0,
