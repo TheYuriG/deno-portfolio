6bf4285d69929055d2f82e6e4d7a7476a17383d2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2023-08-06 10:28:55 -0300

    chore: StyledSubHeader changed to h2 element

--- components/UI/StyledSubHeader.tsx
+++ components/UI/StyledSubHeader.tsx
@@ -1,11 +1,11 @@
 //? Export a subheader that contains all common classes for easy styling and is slightly smaller than Header
 export function StyledSubHeader({ title, id }: { title: string; id?: string }) {
   return (
-    <h1
+    <h2
       id={id}
       class="custom-underline-thick custom-font-scale-large f-as my-4 text-center"
     >
       {title}
-    </h1>
+    </h2>
   );
 }
