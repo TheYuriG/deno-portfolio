6d5d2b0c9e0428b06670d1bb7fa1cfc6d133d952
Yuri Gabriel <yuri@theyurig.com>
2024-09-27 15:20:30 -0300

    chore: formatting

--- types/component-properties/tools/think-binary-search/DifficultyOptions.ts
+++ types/component-properties/tools/think-binary-search/DifficultyOptions.ts
@@ -1,6 +1,5 @@
-
 export type DifficultyOptionsType = {
-    arraySize: 31 | 63 | 127,
-    arrayOrder: 'ordered' | 'uneven' | 'random',
-    arrayAssists: 'both' | 'single' | 'none'
-}
+  arraySize: 31 | 63 | 127;
+  arrayOrder: "ordered" | "uneven" | "random";
+  arrayAssists: "both" | "single" | "none";
+};
