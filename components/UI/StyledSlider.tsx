5478bfba973e159588b7f48a81f66ee0db29d10d
Yuri Gabriel <yuri@theyurig.com>
2024-09-28 18:26:02 -0300

    feat: allow StyledSlider to be disabled, added docs

--- components/UI/StyledSlider.tsx
+++ components/UI/StyledSlider.tsx
@@ -1,10 +1,23 @@
+/**
+ * Creates a stylized slider with the provided parameters.
+ * @param min Defines the lowest point (left) of the slider.
+ * @param max Defines the highest point (right) of the slider.
+ * @param value Current position on the slider.
+ * @param disabled (optional) Wether if this slider can be dragged or not.
+ */
 export function StyledSlider(
-  { min, max, value }: { min: string; max: string; value: string },
+  { min, max, value, disabled }: {
+    min: string;
+    max: string;
+    value: string;
+    disabled?: true;
+  },
 ) {
   return (
     <div class="w-full">
       <input
-        class="styled-slider"
+        class={"styled-slider" +
+          (disabled === true ? " cursor-not-allowed pointer-events-none" : "")}
         type="range"
         min={min}
         max={max}
