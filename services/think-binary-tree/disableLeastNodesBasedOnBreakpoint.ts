e4330f2a8703474781694bcc62f9bc59dcb283f5
Yuri Gabriel <yuri@theyurig.com>
2024-09-29 14:24:34 -0300

    feat: add service to disable numbers

---
+++ services/think-binary-tree/disableLeastNodesBasedOnBreakpoint.ts
@@ -0,0 +1,118 @@
+//? Types
+import type { NumbersWithStatus } from "../../types/component-properties/tools/think-binary-search/NumberWithStatus.ts";
+
+/**
+ * Given an array of type `Array<NumberWithStatus>`, filter all disabled nodes and return the array.
+ * @param array Array to be filtered.
+ * @returns The filtered array without disabled nodes.
+ */
+function filterDisabledIndices(
+  array: Array<NumbersWithStatus>,
+): Array<NumbersWithStatus> {
+  const arrayWithoutDisabledNodes: Array<NumbersWithStatus> = array.filter((
+    item: NumbersWithStatus,
+  ) => item.disabled === false);
+  return arrayWithoutDisabledNodes;
+}
+
+/**
+ * Given an `Array<NumberWithStatus>` and a `searchNumber`, scan the array and return if there are more indices lower than or greater than the `searchNumber`.
+ * @param searchNumber The `number` to be used to search the array for the midway point.
+ * @param arrayWithNumberAndDisabledProperties An `Array<NumberWithStatus>` to be searched through.
+ * @returns "lower" or "greater", indicating if there are more indices lower than or greater than the `searchNumber`.
+ */
+function findIfThereAreMoreIndicesGreaterOrLowerThanSearchNumber(
+  searchNumber: number,
+  arrayWithNumberAndDisabledProperties: Array<NumbersWithStatus>,
+): "lower" | "greater" {
+  function isSearchNumberIsGreaterOrLowerThanMidwayPoint(
+    position: number,
+    arraySize: number,
+  ) {
+    if (position >= arraySize / 2) {
+      return "greater";
+    } else {
+      return "lower";
+    }
+  }
+  const numbersOnlyArray: Array<number> = arrayWithNumberAndDisabledProperties
+    .map((item) => item.number);
+
+  const arraySize = numbersOnlyArray.length;
+
+  let indicesLowerThanSearchNumber = 0;
+  for (let i = 0; i < arraySize; i++) {
+    if (searchNumber > numbersOnlyArray[i]) {
+      break;
+    }
+    indicesLowerThanSearchNumber++;
+  }
+  return isSearchNumberIsGreaterOrLowerThanMidwayPoint(
+    indicesLowerThanSearchNumber,
+    arraySize,
+  );
+}
+
+/**
+ * Given the correct parameters, disable as few indices as possible and then returns the final `Array<NumberWithStatus>`.
+ * @param greaterOrLower Wether if the indices lower than or greater than the `searchNumber` should be disabled.
+ * @param searchNumber The dividing `number` that defines which indices will be disabled.
+ * @param arrayToDisable The `Array<NumberWithStatus>` that will have indices being disabled.
+ * @returns An updated `Array<NumberWithStatus>` with indices disabled based on `greaterOrLower` and `searchNumber`.
+ */
+function disableNodesGreaterThanOrLowerThanSearchNumber(
+  greaterOrLower: "greater" | "lower",
+  searchNumber: number,
+  arrayToDisable: Array<NumbersWithStatus>,
+) {
+  const updatedArrayToDisable = [...arrayToDisable];
+  if (greaterOrLower === "lower") {
+    for (let i = 0; i < updatedArrayToDisable.length; i++) {
+      if (searchNumber < updatedArrayToDisable[i].number) {
+        break;
+      }
+      updatedArrayToDisable[i].disabled = true;
+    }
+  } else {
+    for (let i = updatedArrayToDisable.length - 1; i >= 0; i--) {
+      if (searchNumber > updatedArrayToDisable[i].number) {
+        break;
+      }
+      updatedArrayToDisable[i].disabled = true;
+    }
+  }
+  return updatedArrayToDisable;
+}
+
+/**
+ * Given an `arrayOfUniqueNumbers` of type `Array<NumberWithStatus>` and a `breakpoint`, filter all the disabled indices and then uses the `breakpoint` value to disable as few indices as possible, then returning the updated array.
+ * @param searchNumber A `number` that defines where the array should be split, always favoring the portion with more remaining indices.
+ * @param arrayOfUniqueNumbers An `Array<NumberWithStatus>` of elements that will be split on the breakpoint number.
+ * @returns An `Array<NumberWithStatus>` that with as few indices as possible
+ */
+export function disableLeastNodesBasedOnBreakpoint(
+  searchNumber: number,
+  arrayOfUniqueNumbers: Array<NumbersWithStatus>,
+): Array<NumbersWithStatus> {
+  const filteredArrayWithoutDisabledNumbers: Array<NumbersWithStatus> =
+    filterDisabledIndices(arrayOfUniqueNumbers);
+
+  let midwayPointCounter: number = 0;
+  for (let i = 0; i < filteredArrayWithoutDisabledNumbers.length; i++) {
+    if (searchNumber > filteredArrayWithoutDisabledNumbers[i].number) {
+      midwayPointCounter++;
+    }
+  }
+
+  const leastIndicesGreaterOrLowerThanSearchNumber =
+    findIfThereAreMoreIndicesGreaterOrLowerThanSearchNumber(
+      searchNumber,
+      arrayOfUniqueNumbers,
+    );
+
+  return disableNodesGreaterThanOrLowerThanSearchNumber(
+    leastIndicesGreaterOrLowerThanSearchNumber,
+    searchNumber,
+    arrayOfUniqueNumbers,
+  );
+}
